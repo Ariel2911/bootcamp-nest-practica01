bf918a99bad0fa55bbea30fb8b78f1ca6cc2ee1c
Ariel Barrios <ariel29111978@hotmail.com>
2023-11-03 22:52:19 -0300

    Agrega la carpeta data

---
+++ data/data.ts
@@ -0,0 +1,32 @@
+export const users = [
+  {
+    id: 1,
+    name: '01-Ariel',
+    surname: 'qwe',
+    age: '22',
+  },
+  {
+    id: 2,
+    name: '02-Ariel',
+    surname: 'asd',
+    age: '21',
+  },
+  {
+    id: 3,
+    name: '03-Ariel',
+    surname: 'zxc',
+    age: '20',
+  },
+  {
+    id: 4,
+    name: '04-Ariel',
+    surname: 'rty',
+    age: '12',
+  },
+  {
+    id: 5,
+    name: '05-Ariel',
+    surname: 'fgh',
+    age: '212',
+  },
+];
